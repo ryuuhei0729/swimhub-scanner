bda80334297c77f4d0aa41043e3f8811256a73e2
ryuhei.hosoi <ryuuhei_0729@yahoo.co.jp>
2026-03-01 16:05:33 +0900

    feat: Update app configuration and dependencies for improved functionality
    - Added Expo updates configuration and runtime version policy in app.config.js for better app management.
    - Updated app.json with the correct iOS app ID and project ID for EAS.
    - Enhanced package.json by adding babel-preset-expo and updating expo-updates to the latest version for improved performance and compatibility.
    - Updated .gitignore to include additional files for better version control.

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -21,6 +21,12 @@ if (process.env.NODE_ENV === 'development') {
 
 module.exports = {
   ...baseConfig.expo,
+  updates: {
+    url: 'https://u.expo.dev/d123c26f-8ad7-4505-b6f2-11d82a741b99',
+  },
+  runtimeVersion: {
+    policy: 'appVersion',
+  },
   extra: {
     ...baseConfig.expo.extra,
     supabaseUrl: supabaseUrl,
